08c65672d63a96433c4776a3be5bbbe1f226c9d7
Shubhank92 <57147257+Shubhank92@users.noreply.github.com>
2022-01-18 20:59:37 +0530

    Update service.js

--- src/components/service.js
+++ src/components/service.js
@@ -1,64 +1,40 @@
 import React from 'react';
+import ServiceContent from './servicecontent';
 
-function Service () {
+function Service ( props ) {
+    const {selcName, selectedService} = props;
     return (
         <div className='w-80 center pt6'>
             <h1 className='center w-80 tc'><b>Thrive in today's ever-changing, digital world </b>with connected solutions that simply work</h1>
             <div className="container">
-                <div class="mw9 center ph3-ns">
-                    <div class="cf ph2-ns">
-                        <div class="fl w-100 w-50-ns pa2">
-                        <div class="bg-white pv4"></div>
+                <div className="mw9 center ph3-ns">
+                    <div className="cf ph2-ns">
+                        <div className="fl h-100 w-100 w-50-ns pa2 outline">
+                            <a class="f6 link dim br3 ph3 pv2 mb2 dib white bg-near-black" href="" onClick={(e) => selcName(e.target.innerText)}>Financials</a>
+                            <a class="f6 link dim br3 ph3 pv2 mb2 dib white bg-near-black" href="" onClick={(e) => selcName(e.target.innerText)}>Planning</a>
+                            <a class="f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-gray" href="" onClick={(e) => selcName(e.target.innerText)}>Analytics</a>
+                            <a class="f6 link dim br3 ph3 pv2 mb2 dib white bg-mid-gray" href="" onClick={(e) => selcName(e.target.innerText)}>HR & Payroll</a>
+                        <div className="bg-white pv4"></div>
                         </div>
-                        <div class="fl w-100 w-50-ns pa2">
-                            <div class="bg-white pv4">
+                        <div className="fl w-100 w-50-ns pa2">
+                            <div className="bg-white pv4">
                                 <div>
-                                    <h3>Financials</h3>
-                                    <p>Free up more time to think strategically and guide your organization
-                                        <ul>
-                                            <li><b>Slice and dice your data with</b> real-time, multi-dimensional reporting</li>
-                                            <li><b>Consolidate all your entities in minutes</b>, no more spreadsheets </li>
-                                            <li><b>Automate mundane accounting tasks</b> to increase your efficiency up to 90%</li>
-                                            <li><b>Free your teams to focus on higher value work</b> with AI-powered solutions</li>
-                                        </ul>  
-                                    </p>
-                                    <a href="">Learn about Financials</a>
+                                    <h3 onClick={(e) => selcName(e.target.innerText)}>Financials</h3>
+                                    { selectedService === 'Financials' ? <ServiceContent name={selectedService} /> : null}
                                 </div>
                                 <div>
-                                    <h3>Planning</h3>
-                                    <p>Enable your organization to budget, plan, and quickly adapt to ever-changing conditions
-                                        <ul>
-                                            <li><b>Plan using live financial data</b> via our native, bi-directional connector</li>
-                                            <li><b>Get up and running in days</b> on an intuitive solution, built for finance users</li>
-                                            <li><b>Monitor performance in real-time</b> with interactive reports and dashboards</li>
-                                            <li><b>Reforecast your business in days</b> with powerful what-if scenario modeling</li>
-                                        </ul>  
-                                    </p>
-                                    <a href="">Learn about Planning</a>
+                                    <h3 onClick={(e) => selcName(e.target.innerText)}>Planning</h3>
+                                    { selectedService === 'Planning' ? <ServiceContent name={selectedService} /> : null}
+
                                 </div>
                                 <div>
-                                    <h3>Analytics</h3>
-                                    <p>Get instant, predictive insights to make data-driven decisions
-                                        <ul>
-                                            <li><b>Plug-and-play, native analytics</b> that use live financial and statistical data</li>
-                                            <li><b>Choose from over 200 prebuilt visuals</b>, edit them, or create your own</li>
-                                            <li><b>Transform your decision-making</b> using dimensional, predictive analytics</li>
-                                            <li><b>Build new visuals and dashboards in minutes</b> to explore data on the fly</li>
-                                        </ul>  
-                                    </p>
-                                    <a href="">Learn about Interactive Visual Explorer</a>
+                                    <h3 onClick={(e) => selcName(e.target.innerText)}>Analytics</h3>
+                                    { selectedService === 'Analytics' ? <ServiceContent name={selectedService} /> : null}
+
                                 </div>
                                 <div>
-                                    <h3>HR & Payroll</h3>
-                                    <p>Empower your organization to take care of your greatest asset, your people
-                                        <ul>
-                                            <li><b>SGet instant visibility into your people data</b> for your local or global workforce</li>
-                                            <li><b>Run payroll in a few simple steps</b> with the leading mid-market payroll solution</li>
-                                            <li><b>Sync employee data to Sage Intacct in real-time,</b> no duplicate data entry</li>
-                                            <li><b>Use your dimensions to streamline posting and reporting</b> for payroll journal entries</li>
-                                        </ul>  
-                                    </p>
-                                    <a href="">Learn about HR & People Management</a>
+                                    <h3 onClick={(e) => selcName(e.target.innerText)}>HR & Payroll</h3>
+                                    { selectedService === 'HR & Payroll' ? <ServiceContent name={selectedService} /> : null}
                                 </div>
                             </div>
                         </div>
